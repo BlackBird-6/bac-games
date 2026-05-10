590bb5731e16a1f069055e050fe5869db8cbf105
BlackBird_6 <blackbird6366@gmail.com>
2026-05-10 00:12:20 -0400

    add weirdskribbl.py (for other word lists)

---
+++ analysis.py
@@ -0,0 +1,116 @@
+import sqlite3
+from collections import Counter, defaultdict
+import random
+import time
+
+conn = sqlite3.connect('bac-database/bacap.db')
+cursor = conn.cursor()
+database_data = cursor.execute('''SELECT name from advancements ''')
+
+
+# database_data = cursor.execute('''SELECT name from advancements WHERE name like 'a%' ''')
+# database_data = cursor.execute('''SELECT name from advancements WHERE version like '%1.20%' OR version like '%1.19%' OR version like '%1.18%' OR version like '%1.17%' OR version like '%1.16%' ''')
+
+d = database_data.fetchall()
+advs = [str(adv[0]) for adv in d]
+
+nadvs = ["".join([c.upper() for c in adv if c.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]) for adv in advs]
+nsadvs = ["".join([c.upper() for c in adv if c.lower() in "abcdefghijklmnopqrstuvwxyz0123456789 "]) for adv in advs]
+
+# Below follows a bunch of random analysis
+
+# PRINT ALL LOADED ADVS
+def printAllAdvs():
+    for l in advs:
+        print(l)
+
+# LIST ALL ADVANCEMENTS WHOSE LENGTH IS X
+def printAllAdvsLengthX(X: int):
+    arr = [a for a in nadvs if len(a) == X]
+    print(arr)
+    print(len(arr))
+
+
+# COUNT ALL DUPLICATE ADVS
+def countDuplicates():
+    advs = [a.strip() for a in advs]
+    c = [(a, b) for a, b in Counter(advs).items()]
+    print([cn for cn in c if cn[1] >= 2])
+
+# COUNT ALL SINGLE LETTERS IN WORDS
+def countSingleLetters():
+    for a in nsadvs:
+        a = a.split()
+        for w in a:
+            if len(w) == 1 and w != 'A' and w != 'I':
+                print(" ".join(a))
+                break
+
+def advsWithRepeatedWords():
+    for a in nsadvs:
+        a = a.split()
+        words = []
+        for w in a:
+            if w in words:
+                print(" ".join(a))
+                break
+            words.append(w)
+
+def nonTitleCaseAdvs():
+    for a in advs:
+        a = a.split()
+        for w in a:
+            if w[0] in "abcdefghijklmnopqrstuvwxyz" and len(w) > 3:
+                print(" ".join(a))
+                break
+
+# Sort words by how many times they appear in adv names
+def sortObscurity():
+    words = defaultdict(int)
+    for a in nsadvs:
+        for word in a.split():
+            words[word] += 1
+    print(sorted([(a, b) for a, b in words.items()], key=lambda x: x[1], reverse=True)) # How many times does each word appear
+    obscurity = [(a,
+                  sum(words[w] for w in a.split()),
+                  [(w, words[w]) for w in a.split()]
+                  ) for a in nsadvs]
+    obscurity.sort(key=lambda x: x[1])
+    print(obscurity) # Rank advs by obscurity (words more unique to this advancement)
+
+
+o = open("Text/out.txt", "w+", encoding="UTF-8")
+for a in sorted(advs):
+    o.write(a + "\n")
+    print(a)
+o.close()
+# for a in advs:
+#         a = a.split()
+#         for w in a[1:]:
+#             if w[0] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" and len(w) < 3:
+#                 print(" ".join(a))
+
+# for a in advs:
+#     print(a)
+
+    # if len(o[0].split()) == o[1]:
+    #     print(o)
+
+# for a in nadvs:
+#     a = a.split()
+#     if len(a) == 4:
+#         print([(w, words[w]) for w in a])
+
+
+# time.sleep(1000)
+
+# Sort advs by length letters only
+# nadvs = ["".join([c.upper() for c in adv if c.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]) for adv in advs]
+# res = list(zip(ladvs, [len(l) for l in nadvs]))
+# res.sort(key=lambda x: x[1])
+# for l in res:
+#     print(l)
+
+#
+# # Exit code 1
+# print(advs[1000000])
---
+++ weirdskribbl.py
@@ -0,0 +1,42 @@
+import time
+import random
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("shivamb/10000-most-common-passwords")
+
+# print("Path to dataset files", path)
+
+# print(",".join([str(i) for i in range(100)]))
+
+# str = "AngryCave,Blaze,BlazeHappy,Cave,CaveFaceInTheFace,DisappointedCave,EvilCave,PharaohCave,RealisticCave,SadCave,Thomas,UpsideDownCave,Weathered_Copper_BlocK,Weathered_Copper_BlocK2,Yahya,abduction,advancement,advancement2,aerial_assault_drone,aol,aostralia,assassin,australia,barry,blaze,blueenderman,bruh,cake,camel,camel2,captain_kelrig,captaincoldbeard,cave_spider,celestipede_body,celestipede_head,celestipede_tail,command_block,creeper,chicken,darkhelmet,dennis,diamond_hoe,distortedzombie,dk,drone,ghast,frog,farewell,evoker,enderman,enderghast,enderdragon,elder_guardian,dwarf,gnome,greenenderman,guardian,happy_ghast,happy_ghastling,henry,herobrine,hyso,iron_golem,jeb_nooooo,kneel,laser_blaster,laser_blaster2,lemon_camel,lucas,magmacube,mariowaah,master,mcmovie_sheep,montyshocked,narelle,no,pharaoh,pig,polarbear,pumpkin,rick_astley,sandmonster,sanic,sherifffirearm,skeleton,slime,spider,startrekkin,steve,steve_in_love,supersanic,theworldisending,thinkos,utgy,villager,vindicator,warden,wither,wither_skeleton,yellowenderman,yes,yoda,zombie,zombie_pigman"
+# str = str.replace("_", " ")
+# print(str)
+# print(len(str.split(",")))
+
+letters = [c for c in "abcdefghijklmnopqrstuvwxyz"]
+
+words = []
+for i in letters:
+    for j in letters:
+        for k in letters:
+            for l in letters + [" "]:
+                words.append(f"{i}{j}{k}{l}")
+            
+candidates = ",".join(words)
+
+f = open("Text/words.txt", "r+").read().splitlines()
+res = set()
+
+for w in f:
+    if len(w) == 4 or len(w) == 3:
+        res.add(w)
+
+while len(",".join(res)) < 20000:
+    print(len(",".join(res)))
+    candidate = candidates.split(",").pop(random.randrange(0, len(candidates.split(","))))
+    res.add(candidate)
+
+res = list(res)
+print(",".join(res))
+print(len(res))
